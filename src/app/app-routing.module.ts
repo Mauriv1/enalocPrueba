28c97197c0a0ed29ba822886fe8a2a3d44dfdbdc
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-10-25 04:42:58 -0500

    commit-contenido-inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ActuacionDoblajeComponent } from './components/actuacion-doblaje/actuacion-doblaje.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NuestrosTalentosComponent } from './components/nuestros-talentos/nuestros-talentos.component';
+import { PowerKidsComponent } from './components/power-kids/power-kids.component';
+import { PowerTeensComponent } from './components/power-teens/power-teens.component';
+import { SesionesOnlineComponent } from './components/sesiones-online/sesiones-online.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', pathMatch:'full', redirectTo:'home'},
+  {path:'inicio', component: InicioComponent},
+  {path:'actuacion-doblaje', component: ActuacionDoblajeComponent},
+  {path:'sesiones-online', component: SesionesOnlineComponent},
+  {path:'nuestros-talentos', component: NuestrosTalentosComponent},
+  {path:'power-kids', component: PowerKidsComponent},
+  {path:'power-teens', component: PowerTeensComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
