1173fb65aebaa9e045e848e6c5e73410fd280db6
Mauriv1 <Mauriv1@users.noreply.github.com>
2022-10-30 19:19:30 -0600

    commit-contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ActuacionDoblajeComponent } from './components/actuacion-doblaje/actuacion-doblaje.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { NuestrosTalentosComponent } from './components/nuestros-talentos/nuestros-talentos.component';
-import { PowerKidsComponent } from './components/power-kids/power-kids.component';
-import { PowerTeensComponent } from './components/power-teens/power-teens.component';
+
+
 import { SesionesOnlineComponent } from './components/sesiones-online/sesiones-online.component';
 
 const routes: Routes = [
@@ -12,9 +12,7 @@ const routes: Routes = [
   {path:'inicio', component: InicioComponent},
   {path:'actuacion-doblaje', component: ActuacionDoblajeComponent},
   {path:'sesiones-online', component: SesionesOnlineComponent},
-  {path:'nuestros-talentos', component: NuestrosTalentosComponent},
-  {path:'power-kids', component: PowerKidsComponent},
-  {path:'power-teens', component: PowerTeensComponent}
+  {path:'nuestros-talentos', component: NuestrosTalentosComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { ActuacionDoblajeComponent } from './components/actuacion-doblaje/actuacion-doblaje.component';
 import { SesionesOnlineComponent } from './components/sesiones-online/sesiones-online.component';
 import { NuestrosTalentosComponent } from './components/nuestros-talentos/nuestros-talentos.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FootComponent } from './components/foot/foot.component';
-import { PowerKidsComponent } from './components/power-kids/power-kids.component';
-import { PowerTeensComponent } from './components/power-teens/power-teens.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     InicioComponent,
     ActuacionDoblajeComponent,
     SesionesOnlineComponent,
     NuestrosTalentosComponent,
     NavComponent,
     FootComponent,
-    PowerKidsComponent,
-    PowerTeensComponent
+    ContactoComponent,
   ],
   imports: [
     BrowserModule,
